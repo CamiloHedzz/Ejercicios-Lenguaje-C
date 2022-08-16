28d99df7312f0e96a0996a3ffb7a47e7a6cef3ac
CamiloHedzz <111331587+CamiloHedzz@users.noreply.github.com>
2022-08-15 22:30:17 -0500

    Add files via upload

---
+++ mm_lib.h
@@ -0,0 +1,38 @@
+/****************************************************************
+ *                   DEFINICION DE LAS FUNCIONES                 *
+ *          UTILIZADAS EN LA CLASE PRINCIPAL (mm_main.c)        *
+ * **************************************************************/
+
+#ifndef MM_LIB_H_INCLUDED
+#define MM_LIB_H_INCLUDED
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <ncurses.h>
+
+
+void multiplicacion();
+
+void numeroDias();
+
+void distanciaPuntos();
+
+void numerosNaturales();
+
+void trianguloRectangulo();
+
+void realizarMatriz();
+
+void numerosAleatorios();
+
+void imprimirMatriz(int a[][7]);
+
+void sumaMatrices();
+
+void restaMatrices();
+
+void productoMatrices();
+
+
+
+#endif
---
+++ mm_main.c
@@ -0,0 +1,41 @@
+/****************************************************************
+ *                                                              *
+ *                      DOCENTE: John Corredor                  *
+ *                      TOPIC: Primer taller C                  *
+ *                  UNIVERSIDAD: Sergio Arboleda                *
+ *              AUTOR: Juan Camilo Hernández Ibáñez             *
+ *          ASIGNATURA: Parallel and Distributed Computing      *
+ *                                                              *
+ * *************************************************************/
+
+#include "mm_lib.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <ncurses.h>
+
+
+int main (int argc, char *argv[]){
+    
+    multiplicacion();
+
+    numeroDias();
+
+    distanciaPuntos();
+
+    numerosNaturales();
+
+    trianguloRectangulo();
+    
+    realizarMatriz();
+
+    numerosAleatorios();
+    
+    sumaMatrices();
+
+    restaMatrices();
+
+    productoMatrices();
+
+    return 0;
+}
